8d642176ff22ff78b4cde38194ec239043f7dc2a
Robin Li <lirobinxc@gmail.com>
2020-12-10 18:01:18 -0500

    modified components

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) =>
-    <div className="tc">
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={user.id}
-              name={user.name}
-              email={user.email}
-              />
-          );
-        })
-      }
-    </div>
+  <div className="tc">
+    {
+      robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+            />
+        );
+      })
+    }
+  </div>
 
 export default CardList;
